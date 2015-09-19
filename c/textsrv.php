1045070bde0c65094ac89e38b7dbdf92d7cbe5c7
don kretz <dakretz@gmail.com>
2015-09-18 17:28:46 -0700

    Completed query by version

--- c/textsrv.php
+++ c/textsrv.php
@@ -5,15 +5,17 @@ require_once "pinc/dpinit.php";
 $projectid = ArgProjectid();
 $pagename  = ArgPageName();
 $roundid   = Arg("roundid");
+$version   = Arg("version");
 
 $pg = new DpPage($projectid, $pagename);
+$text = $pg->VersionText($version);
 
-if($roundid) {
-	$text = $pg->PhaseText($roundid);
-}
-else {
-	$text = $pg->PhaseText($pg->Phase());
-}
+//if($roundid) {
+//	$text = $pg->PhaseText($roundid);
+//}
+//else {
+//	$text = $pg->PhaseText($pg->Phase());
+//}
 
 echo
 "<!DOCTYPE html>
